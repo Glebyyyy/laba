e77a0fdba02b327de8cd3c06f89f60f0c2153f95
Gleb <gl.koptsev@yandex.ru>
2023-07-25 18:25:28 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int operand1 = scanner.nextInt(); //считывает число
+/*Метод next() класса Scanner считывает строку,
+а метод charAt(0) позволяет взять первый символ в этой строке.
+ так мы получаем операцию, которую нужно выполнить*/
+        char operation = scanner.next().charAt(0);
+    }
+}
---
+++ src/Scanner.java
@@ -0,0 +1,11 @@
+import java.io.InputStream;
+
+public class Scanner {
+    public Scanner(InputStream in) {
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
